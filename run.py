8f3f9e0c7cf62bf837e99bcb29a499c8a907919c
Ubuntu <ubuntu@ip-172-31-89-6.ec2.internal>
2019-08-01 15:34:56 +0000

    updating contact form

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.secret_key = "some_secret"
 
 
 @app.route('/')
@@ -30,8 +31,11 @@ def about_saraSweets(saraSweets_name):
     return render_template("saraSweets.html", saraSweets=saraSweets)
 
 
-@app.route('/contact')
+@app.route('/contact', methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have receive your message!".format(
+              request.form["name"]))
     return render_template("contact.html", page_title="Contact us!")
 
 
